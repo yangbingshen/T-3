c1c81797e44f3e3ef2828db8af12cfeb5f00c3ec
yangb <yangb@LAPTOP-29N9KJRU>
2022-02-08 16:11:29 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿// C# program to convert canadian dollar to US$
+// Bingshen Yang
+// 02/08/2022
+using System;
+
+namespace T_3
+{
+    class Program
+    {
+        public static void Main()
+        {
+           
+            int inc_value;
+
+           
+            while (true)
+            {
+               
+                Console.Write("Enter increament value between 5 and 25 :");
+                inc_value = Convert.ToInt32(Console.ReadLine());
+                
+                if (inc_value >= 5 && inc_value <= 25)
+                {
+                    break;
+                }
+                
+                Console.WriteLine("increament value must be between 5 and 25,, try again");
+            }
+
+            
+            double CAD = 0, USD;
+
+            Console.WriteLine("CAD   US$");
+            Console.WriteLine("-------------");
+
+           
+            do
+            {
+              
+                USD = CAD * 0.792367;
+             
+                Console.WriteLine(CAD + "    " + USD);
+
+              
+                CAD += inc_value;
+            }
+            while (CAD <= 200);
+        }
+    }
+}
